807b012e9145442cfa8adb431c4286df0128b42b
Steve Hou <stevehoho@hotmail.com.tw>
2022-12-24 14:18:10 +0800

    username

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/",methods=['GET','POST'])
 def index():
     return render_template('index.html')
+
+@app.route('/name/<username>')
+def show_user(username):
+    return f"<h3>您的姓名是<strong>(username)</strong></h3>"
