ed412fda4f097439e5d8ec07e24a781baa4c6156
Steve Hou <stevehoho@hotmail.com.tw>
2022-12-24 15:09:29 +0800

    modify

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-from flask import Flask, render_template,request
+from flask import Flask,render_template,request
 
 app = Flask(__name__)
 
 @app.route("/",methods=['GET','POST'])
-def index():
+def index():    
     return render_template('index.html')
 
 @app.route('/name/<username>')
 def show_user(username):
-    return f"<h3>您的姓名是<strong>(username)</strong></h3>"
+    return f"<h3>您的姓名是<strong>{username}</strong></h3>"
 
 @app.route('/login')
 def login():
